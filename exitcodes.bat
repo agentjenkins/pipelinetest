20bc561c227f3ede67e9f1830ce0f823ce650073
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2018-01-26 15:42:33 +0100

    exit codes

--- exitcodes.bat
+++ exitcodes.bat
@@ -2,4 +2,12 @@ ECHO ========== Start anaysis of NCover Data ==========
 cd "%WORKSPACE%\Binaries\UnitTests"
 ECHO "The ERRORLEVEL IS %ERRORLEVEL%"
 SET ERRORLEVEL=3
+IF %ERRORLEVEL% EQ 3 (
+  ECHO "Coverage threshold not met"
+  EXIT 1
+)
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO "Ncover reporting failed"
+  EXIT 1
+)
 ECHO ========== Anaysis of NCover Data finished ==========
