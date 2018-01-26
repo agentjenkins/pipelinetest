f5cb203a4fa66cbd503de2ed6398def8a37de4ad
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2018-01-26 15:36:16 +0100

    exit codes

--- exitcodes.bat
+++ exitcodes.bat
@@ -1,4 +1,5 @@
 ECHO ========== Start anaysis of NCover Data ==========
 cd "%WORKSPACE%\Binaries\UnitTests"
+ECHO %ERRORLEVEL%
 ERRORLEVEL=3
 ECHO ========== Anaysis of NCover Data finished ==========
