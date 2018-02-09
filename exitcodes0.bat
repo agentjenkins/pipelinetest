0e3206f24699e462d4a61a17d95ab2d65db9a7af
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2018-02-09 16:12:18 +0100

    sample files

---
+++ exitcodes0.bat
@@ -0,0 +1,11 @@
+ECHO ========== Start anaysis of NCover Data ==========
+cd "%WORKSPACE%\Binaries\UnitTests"
+"C:\Program Files (x86)\NCover\NCover.Reporting.exe" //cr "%WORKSPACE%\Build\DDM.build.Verification.NCoverReporting.config"
+IF %ERRORLEVEL% EQ 3 (
+  ECHO "Coverage threshold not met"
+) ELSE IF %ERRORLEVEL% NEQ 0 (
+  ECHO "Ncover reporting failed"
+)
+ECHO ========== Anaysis of NCover Data finished ==========
+ECHO "ERRORLEVEL IS %ERRORLEVEL%"
+EXIT %ERRORLEVEL%
